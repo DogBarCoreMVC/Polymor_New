ca1a7d303970eaaf1a823e3f66ee491d814583a9
GitDogBar <Dogbar>
2021-08-15 16:37:33 +0700

    Add project files.

---
+++ Animal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymor_New
+{
+    public class Animal
+    {
+        public void Goes()
+        {
+            Console.WriteLine("Gose like an Animal");
+        }
+    }
+    public class Dog : Animal
+    {
+        /*public void Goes()//การสร้าง method มาแบบนี้จะเป็นการสร้าง method ออกมาซ้ำกันกับ method ที่อยู่ในคลาสแม่ (แต่ทำการรันได้ตามปกติไม่มี error) ข้อแนะนำของ visual studio คือการให้ใส่ key word "new" นำหน้า void 
+        {
+            Console.WriteLine("Class Dog Woof!!");
+        }*///ตามตัวอย่าง
+
+        public new void Goes()
+        {
+            Console.WriteLine("Class Dog Woof!!");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Polymor_New
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.Goes();
+
+            Dog dog = new Dog();
+            dog.Goes();
+        }
+    }
+}
